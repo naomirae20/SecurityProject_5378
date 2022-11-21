e053482c2d552956a77703794e56b911bbbca2db
Jian <jbasravi@gmail.com>
2022-11-21 09:37:28 -0600

    Added tipSelection(). Returns a LinkedList of 0, 1, or 2 block objs. When searching for tips, the radius does not expand unitl tips are found. Instead it is possible for a node to not have any tips if there are not any nodes close enough to it (as stated in the tangleCV article. We might want to change this (or maybe not).

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.lang.Math;
+import java.util.Random;
 
 public class Graph {
     public int difficulty = 4;
+    public double searchRadius = 4.0;
 
     private final ArrayList<Block> graph = new ArrayList<Block>();
     private final ArrayList<LinkedList<Block>> neighborLists = new ArrayList<>();
@@ -23,8 +26,37 @@ public class Graph {
     } //may not need
 
     public LinkedList<Block> tipSelect(int x, int y) {
-        //TODO tip selection
-        return null;
+        if (graph.size() == 0) { return null; }
+        LinkedList<Block> tips = new LinkedList<Block>();
+        LinkedList<Block> possibleTips = new LinkedList<Block>();
+        for (Block b : graph) {
+            double distance = 
+                Math.sqrt(Math.pow((x - b.x), 2) + Math.pow((y - b.y), 2));
+            if ((distance <= searchRadius) && (b.validatorCount() < 2)) {
+                possibleTips.add(b);
+            }
+        }
+        if (possibleTips.size() == 0) { return null; }
+        if (possibleTips.size() <= 2) {
+            possibleTips.get(0).incrementValidators();
+            tips.add(possibleTips.get(0));
+            if (possibleTips.size() == 2) {
+                possibleTips.get(1).incrementValidators();
+                tips.add(possibleTips.get(1));
+            }
+            return tips;
+        }
+        Random rand = new Random();
+        int r1, r2;
+        r1 = r2 = rand.nextInt(possibleTips.size());
+        while (r1 == r2) {
+            r2 = rand.nextInt(possibleTips.size());
+        }
+        possibleTips.get(r1).incrementValidators();
+        possibleTips.get(r2).incrementValidators();
+        tips.add(possibleTips.get(r1));
+        tips.add(possibleTips.get(r2));
+        return tips;
     }
 
     public void addBlock(String data, int x, int y) {
