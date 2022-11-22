8d95c0a1d41069e67c3264f94223ff0972634296
Avery <averyvan@gmail.com>
2022-11-21 18:50:38 -0600

    Added Winternitz tip validation

--- src/Block.java
+++ src/Block.java
@@ -2,18 +2,24 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
+import java.util.Random;
 
 class Block
 {
     final int x;
     final int y;
+    final String data;
+    final double timestamp = Instant.now().getEpochSecond();
+    byte[][] publicKeys = new byte[32][];
+    byte[][] signature = new byte[32][];
 
-    private final String data;
-    private final double timestamp = Instant.now().getEpochSecond();
-    private String hash;
+    private final byte[][] privateKeys = new byte[32][];
     private int nonce = 0;
+
+    private byte[] hash;
+
     private int validators = 0;
-    
+
     // set local variables
     public Block(String data, int x, int y) {
         this.data = data;
@@ -23,23 +29,18 @@ class Block
     }
 
     // calculate SHA256 hash of (sum of all local vars)
-    public String calculateHash() {
+    public byte[] calculateHash() {
         String str = x + y + timestamp + data + nonce;
 
-        MessageDigest md = null;
-        try {
-            md = MessageDigest.getInstance("SHA-256");  // grabs the SHA-256 algorithm
-        } catch (NoSuchAlgorithmException e) {
-            System.out.println("HASHING ERROR: " + e.getMessage());
-        }
-        assert md != null;
-        byte[] hashBytes = md.digest(str.getBytes(StandardCharsets.UTF_8)); // gets SHA-256 hash in bytes
+        return SHA256(str.getBytes(StandardCharsets.UTF_8)); // gets SHA-256 hash in bytes
+    }
 
-        StringBuilder result = new StringBuilder();     // StringBuilder to save re-allocating the String every loop
-        for (byte b : hashBytes) {                      // Convert each byte to hex
-            result.append(String.format("%02x", b));
-        }
-        return result.toString(); // return hash as hexadecimal string
+    public byte[] getHash() {
+        return hash;
+    }
+
+    public String getHashString() {
+        return bytesToHex(hash);
     }
     
     // PoW
@@ -47,12 +48,11 @@ class Block
         assert(difficulty > 0 && difficulty < 20);
         String offset = new String(new char[difficulty]).replace("\0", "0"); // makes char array, replaces null terms with 0s, makes string of 0s
 
-        while(!hash.startsWith(offset)) {
+        while(!getHashString().startsWith(offset)) {
             nonce++;
             hash = calculateHash();
         }
-
-        System.out.println("Block mined: " + hash);
+        System.out.println("Block mined: " + bytesToHex(hash));
     }
 
     public void incrementValidators() {
@@ -62,5 +62,51 @@ class Block
     public int validatorCount() {
         return validators;
     }
-  
+
+    // generates signature and keys
+    public void generateKeys() {
+        Random rand = new Random();
+        for (int i = 0; i < 32; i++) {
+            privateKeys[i] = new byte[32];
+            rand.nextBytes(privateKeys[i]); //generate 256-bit private keys
+        }
+
+        for (int i = 0; i < 32; i++) {
+            publicKeys[i] = privateKeys[i];
+            for (int j = 0; j < 256; j++) {             // hash public key 256 times
+                publicKeys[i] = SHA256(publicKeys[i]);
+                if (j == 255-(hash[i] & 0xff))          // grab signature when has hashed enough times
+                    signature[i] = publicKeys[i];       // signature is hashed 256-N times (N is hash[i])
+            }
+        }
+    }
+
+    static String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();     // StringBuilder to save re-allocating the String every loop
+        for (byte b : bytes) {                      // Convert each byte to hex
+            result.append(String.format("%02x", b));
+        }
+        return result.toString(); // return hash as hexadecimal string
+    }
+
+    public static byte[] SHA256(byte[] input) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");  // grabs the SHA-256 algorithm
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("HASHING ERROR: " + e.getMessage());
+        }
+        assert md != null;
+
+        return md.digest(input); // gets SHA-256 hash in bytes
+    }
+
+    public String toString() {
+        return "/----------------\\\n" +
+                "At ("+x+", " +y+")\n" +
+                "data: " + data + "\n" +
+                "timestamp: " + timestamp + "\n" +
+                "validators: " + validators + "\n" +
+                "\\----------------/\n";
+    }
 }
--- src/Graph.java
+++ src/Graph.java
@@ -1,34 +1,43 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.lang.Math;
 import java.util.Random;
 
-public class Graph {
-    public int difficulty = 4;
+public class Graph
+{
+    public int difficulty = 4;  // arbitrary
     public double searchRadius = 4.0;
 
-    private final ArrayList<Block> graph = new ArrayList<Block>();
+    private final ArrayList<Block> graph = new ArrayList<>();
     private final ArrayList<LinkedList<Block>> neighborLists = new ArrayList<>();
 
-    public Block getLatestBlock() {
-        return graph.get(graph.size() - 1);
-    } //may not need
+    public Block addBlock(String data, int x, int y) {
+        Block block = new Block(data, x, y);            // make new block
+        LinkedList<Block> neighbors = tipSelect(x, y);  // tip select
+
+        verifySignatures(neighbors);                    // tip validation of W-OTS signatures
+
+        block.mineBlock(difficulty);                    // perform PoW
+        block.generateKeys();                           // generate W-OTS signature and keys
+
+        graph.add(block);                               // add block to graph
+        neighborLists.add(neighbors);                   // add neighbors to adjacency list
+
+        return block;
+    }
+
     public LinkedList<Block> getNeighbors(Block block) {
         return neighborLists.get(graph.indexOf(block));
     }
     public LinkedList<Block> getNeighbors(int index) {
         return neighborLists.get(index);
     }
-    public int addNeighbor(Block origin, Block target) {
-        LinkedList<Block> neighbors = getNeighbors(origin);
-        neighbors.add(target);
-        return neighbors.size();
-    } //may not need
 
     public LinkedList<Block> tipSelect(int x, int y) {
         if (graph.size() == 0) { return null; }
-        LinkedList<Block> tips = new LinkedList<Block>();
-        LinkedList<Block> possibleTips = new LinkedList<Block>();
+        LinkedList<Block> tips = new LinkedList<>();
+        LinkedList<Block> possibleTips = new LinkedList<>();
         for (Block b : graph) {
             double distance = 
                 Math.sqrt(Math.pow((x - b.x), 2) + Math.pow((y - b.y), 2));
@@ -59,14 +68,29 @@ public class Graph {
         return tips;
     }
 
-    public void addBlock(String data, int x, int y) {
-        Block block = new Block(data, x, y);            // make new block
-        LinkedList<Block> neighbors = tipSelect(x, y);  // tip select
-
-        block.mineBlock(difficulty);                    // perform PoW
-
-        graph.add(block);                               // add block to graph
-        neighborLists.add(neighbors);                   // add neighbors to adjacency list
+    public boolean verifySignatures(LinkedList<Block> blocks) {
+        if (blocks == null) return true;
+        for (Block block : blocks) {
+            if (!validateSignature(block))
+                return false;   // a signature didn't match
+        }
+        return true;    // all signatures match
     }
+    public boolean validateSignature(Block block) {
+        byte[][] sig = new byte[32][];
+        for (int i = 0; i < block.signature.length; i++)
+            sig[i] = block.signature[i].clone();                    // make local copy of signature
 
+        for (int i = 0; i < 32; i++) {                              // for each 256-bit value
+            for (int j = 0; j < (block.getHash()[i] & 0xff); j++) { // hash sig N times to get publicKey
+                sig[i] = Block.SHA256(sig[i]);
+            }
+            if (!Arrays.equals(sig[i], block.publicKeys[i])) {      // check sig matches publicKey
+                System.out.println("\nWARNING: Validation failed on this block: \n" + block);
+                return false;
+            }
+        }
+        System.out.println("Validation succeeded on this block: \n" + block);
+        return true; // signature matches
+    }
 }
